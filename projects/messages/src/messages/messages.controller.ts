769a00551de7cc9270a530a33f51fbc09387202d
CLV\thong.le <thongle.clv@gmail.com>
2023-12-18 15:31:13 +0700

    add DI

--- projects/messages/src/messages/messages.controller.ts
+++ projects/messages/src/messages/messages.controller.ts
@@ -1,17 +1,36 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  NotFoundException,
+} from '@nestjs/common';
+import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
-  @Get()
-  listMessage() {}
+  constructor(public messagesService: MessagesService) {}
 
-  @Post()
-  createMessage(@Body() body: any) {
-    console.log(body);
+  @Get()
+  listMessages() {
+    return this.messagesService.findAll();
   }
 
-  @Get(':id')
-  getMessage(@Param('id') id: string) {
-    console.log(id);
+  @Post()
+  createMessage(@Body() body: CreateMessageDto) {
+    return this.messagesService.create(body.content);
+  }
+
+  @Get('/:id')
+  async getMessage(@Param('id') id: string) {
+    const message = await this.messagesService.findOne(id);
+
+    if (!message) {
+      throw new NotFoundException('message not found');
+    }
+
+    return message;
   }
 }
