ec2784df813c001dca834d367391d6325cfa250e
Thong Le <thong.le@cyberlogitec.com>
2023-12-22 09:01:22 +0700

    update auth

--- projects/events/src/auth/auth.controller.ts
+++ projects/events/src/auth/auth.controller.ts
@@ -1,14 +1,25 @@
-import { Controller, Post, Request, UseGuards } from "@nestjs/common";
+import { Controller, Get, Post, Request, UseGuards } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
+import { AuthService } from "./auth.service";
 
 @Controller('auth')
 export class AuthController {
+  constructor(
+    private readonly authService: AuthService
+  ) { }
+
   @Post('login')
   @UseGuards(AuthGuard('local'))
   async login(@Request() request) {
     return {
       userId: request.user.id,
-      token: 'the token will go here'
+      token: this.authService.getTokenForUser(request.user)
     }
+  }
+
+  @Get('profile')
+  @UseGuards(AuthGuard('jwt'))
+  async getProfile(@Request() request) {
+    return request.user;
   }
 }
