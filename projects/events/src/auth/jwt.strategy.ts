bd36a5971379177b04d868242ff194d082220511
Thong Le <thong.le@cyberlogitec.com>
2023-12-22 09:39:36 +0700

    validate token

--- projects/events/src/auth/jwt.strategy.ts
+++ projects/events/src/auth/jwt.strategy.ts
@@ -19,6 +19,8 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: any) {
-    return await this.userRepository.findOne(payload.sub);
+    return await this.userRepository.findOne({
+      where: { id: payload.sub },
+    });
   }
 }
